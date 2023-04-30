406bfa78ae69edad9a7ef376dfcbb0cab8ead506
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-04-30 17:30:08 +0530

    Added Search Bar

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,60 @@
+import { FormEventHandler, LegacyRef, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 const HomePage: React.FC = () => {
+  const navigate = useNavigate();
+  const searchRef: LegacyRef<HTMLInputElement> = useRef(null);
+
+  const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const id = searchRef.current?.value;
+
+    if (!id) {
+      return;
+    }
+
+    navigate({
+      pathname: `/data/${id}`,
+    });
+  };
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="container mx-auto flex flex-col items-center">
+      <div className="relative w-full max-w-4xl mt-40">
+        <form onSubmit={handleSubmit}>
+          <input
+            type="search"
+            id="search-dropdown"
+            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Enter Patient ID"
+            ref={searchRef}
+            required
+          />
+
+          <button
+            type="submit"
+            className="absolute top-0 right-0 p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 "
+          >
+            <svg
+              aria-hidden="true"
+              className="w-5 h-5"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+            <span className="sr-only">Search</span>
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
