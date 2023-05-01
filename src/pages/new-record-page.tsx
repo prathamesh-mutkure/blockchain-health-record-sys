39983d9091c099aee23abfcaf137a67bf7f564c5
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-05-01 11:00:21 +0530

    Structural Changes

--- src/pages/new-record-page.tsx
+++ src/pages/new-record-page.tsx
@@ -1,39 +1,5 @@
-import React, { HTMLInputTypeAttribute } from "react";
-
-interface CustomInputProps {
-  label: string;
-  id: string;
-  placeholder: string;
-  type: HTMLInputTypeAttribute;
-  required?: boolean;
-}
-
-const CustomInput: React.FC<CustomInputProps> = ({
-  label,
-  id,
-  placeholder,
-  type,
-  required,
-}) => {
-  return (
-    <div className="relative z-0 w-full mb-6 group">
-      <label
-        htmlFor="email"
-        className="block mb-2 text-sm font-medium text-gray-900"
-      >
-        {label}
-      </label>
-      <input
-        type={type}
-        id={id}
-        name={id}
-        className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
-        placeholder={placeholder}
-        required={required}
-      />
-    </div>
-  );
-};
+import CustomInput from "components/view-record/app/inputs/custom-input";
+import React from "react";
 
 const NewRecordPage: React.FC = () => {
   return (
